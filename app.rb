ea619c442eba0a86c0133b2fc064f66ab3d134a4
Carlos.Peña <mycalesis@gmail.com>
2013-04-27 20:54:13 +0300

    reference view

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'rest_client'
 require 'json'
 require 'namae'
 
+require './functions'
+
 DB = 'http://localhost:5984/euptychiina'
 
 get '/' do
@@ -37,71 +39,9 @@ get '/contact' do
 end
 
 
-def get_data_from_id(id)
-    resp = RestClient.get "#{DB}/" + id
-    obj = JSON.parse(resp)
-    response = convert_obj_to_citation(obj)
-    return response
-end
+get '/ref/:myid' do
+    myid = "#{params[:myid]}"
+    @citation = get_data_from_myid(myid) 
 
-def convert_obj_to_citation(obj)
-    output  = "<b><a href='ref/'" + obj['myid'] + "'>";
-    output += format_authors(obj);
-    output += "</a></b> ";
-
-    if( obj['year'] )
-        output += obj['year'] + ". "
-    end
-
-    if( obj['title'] )
-        output += obj['title'] + ". "
-    end
-
-    if( obj['journal']['name'] )
-        output += '<i>' + obj['journal']['name'] + "</i>, "
-    end
-
-    if( obj['journal']['volume'] )
-        output += '<b>' + obj['journal']['volume'] + "</b>"
-    end
-
-    if( obj['journal']['issue'] )
-        output += '(' + obj['journal']['issue'] + ")"
-    end
-
-    if( obj['journal']['pages'] )
-        output += " " + obj['journal']['pages'].gsub("--", "-") + "."
-    end
-
-    if( obj['doi'] )
-        output += " <a href='http://dx.doi.org/"
-        output += obj['doi'] + "'>doi:" + obj['doi'] + "</a>"
-    end
-
-    return output
-end
-
-
-def format_authors(obj)
-    n_authors = "";
-
-    obj['author'].each do |author|
-        if( author['given'] && author['family'] )
-            n_authors += author['family'] + " ";
-            given = author['given'].split(" ");
-            given.each do |name|
-                name = name.gsub(".", "").strip[0,1]
-                n_authors += name + ", "
-            end
-
-        elsif author['name']
-            n = Namae.parse author['name']
-            given = n[0].given.strip.gsub(".", "")[0,1];
-            n_authors += n[0].family + " " + given + ", ";
-        end
-    end
-
-    n_authors  = n_authors.strip.sub /,$/, ""
-    n_authors += '.'
-    return n_authors
+    haml :ref
 end
